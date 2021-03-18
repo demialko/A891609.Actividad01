8134bad046cb545c0f9762c659319ee4560c93e2
demialko <demialko@gmail.com>
2021-03-18 08:26:58 -0300

    Inicio de ejercicio
    Commit de la primer actividad planteada.

---
+++ A891609.Actividad01/A891609.Actividad01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A891609.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!\nIngrese una tecla para que finalice el programa");
+            Console.ReadKey();
+        }
+    }
+}
